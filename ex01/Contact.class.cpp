3a0889f1eb4bc5aa1bf2345aa43b0af701296630
KevinPortatil <kkevinlunaa95@gmail.com>
2024-09-11 15:13:02 +0200

    all

---
+++ ex01/Contact.class.cpp
@@ -0,0 +1,20 @@
+#include "Contact.class.hpp"
+
+/*
+Constructor:
+int _index, 
+std::string _first_name, 
+std::string _last_name, 
+std::string _nickname, 
+std::string _phone_number
+*/
+Contact::Contact(void)
+{
+    return;
+};
+
+
+
+Contact::~Contact(void) {
+    return;
+};
